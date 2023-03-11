240043b0f5436936afa3643818ce1127fe540983
Rashad Adams <Rashaddms@gmail.com>
2023-03-11 18:22:15 -0500

    Updating loading screen to delay GitHubBornOn component
    Same as the coins response

--- src/GitHubBornOn.js
+++ src/GitHubBornOn.js
@@ -5,11 +5,15 @@ export const GitHubBornOn = () => {
     // created myInfo variable and set to an empty array
     const [myInfo, updateBornOn] = useState([]);
 
+    // Create a varible for loading
+    const [loading, updateLoading] = useState(true);
+
     // Function call to API
     const fetchMyInfo = async () => {
         const data = await API.get("cryptoapi", `/born`);
         updateBornOn(data.myInfo)
         console.log(data);
+        updateLoading(false)
     };
 
     // calls fetchMyInfo function when component loads and sets to an empty array
@@ -17,9 +21,13 @@ export const GitHubBornOn = () => {
         fetchMyInfo();
     }, []);
 
+    {loading && <h1>Loading ....</h1>}
+
     
     return (
+        !loading &&
         <>
-        <h1>This Github User {myInfo.login}  was born on {myInfo.created_at} </h1>
+         <h2>This Github User {myInfo.login}  was born on {myInfo.created_at} </h2>
         </>
-)};
+    )
+};
